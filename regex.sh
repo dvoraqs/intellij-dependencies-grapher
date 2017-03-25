b6af8f229c85c59a7979d64c828796a720b73ae1
John Beaumier <jbeaumie@thoughtworks.com>
2017-03-24 17:05:13 -0700

    fixed input and output resolution

--- regex.sh
+++ regex.sh
@@ -1,7 +1,7 @@
-INPUT_FILE=${VAR1:-"shared_dependencies_original.xml"}
-OUTPUT_FILE=${VAR2:-"result.xml"}
+INPUT_FILE=${1:-"shared_dependencies_original.xml"}
+OUTPUT_FILE=${2:-"result.xml"}
 
-# Gets rid of lines for jar files
+# Gets rid of lines for archived files
 sed "s/.*\/src.zip!\/.*//g" $INPUT_FILE > $OUTPUT_FILE
 
 echo "Check it out: ${OUTPUT_FILE}"
