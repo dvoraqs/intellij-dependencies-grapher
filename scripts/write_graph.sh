42f19f02ded55b102b42eb5a86a02dfbb2ee9a05
John Beaumier <jbeaumie@thoughtworks.com>
2017-03-26 22:06:00 -0700

    using variable for jar file location

--- scripts/write_graph.sh
+++ scripts/write_graph.sh
@@ -1,9 +1,10 @@
 INPUT_FILE=${1:-"result.txt"}
 OUTPUT_FILE=${2:-"/Users/dvoraqs/Documents/Neo4j/shared.graphdb"}
+JAR_FILE="neo4j-java/target/dependency-graph-0.0.1-SNAPSHOT.jar"
 
 rm -rf $OUTPUT_FILE
 
-java -jar neo4j-java/target/dependency-graph-0.0.1-SNAPSHOT.jar $INPUT_FILE $OUTPUT_FILE | grep 'relationships'
+java -jar $JAR_FILE $INPUT_FILE $OUTPUT_FILE | grep 'relationships'
 
 cd neo4j-java
 mvn clean | grep 'SUCCESS|FAILURE'
